bf6e1c6f01216e8e1f92654483eda5a94c3f8eb5
Adauto Pachêco <guest@BULLSEYE-debian-live-builder>
2024-08-05 10:45:16 -0300

    funções cliente e servidor

--- client_server.py
+++ client_server.py
@@ -1,19 +1,27 @@
-class Fracao:
-    def __init__(self, num, den):
-        self.numerador = num
-        if den is not 0:
-            self.denominador = den
-
-        else:
-            self.denominador = 1
-
-    def divisao(self):
-        return self.numerador/self.denominador
+import socket
 
 
-fracao = Fracao(1, 4)
+def TCPclient(host, port):
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCKS_STREAM)
+    tcp_client.connect((host, port))
+    status = tcp_client.send(b'message')
+    if status > 0:
+        data = tcp_client.recv(1024)
+
+    tcp_client.close()
 
 
-print(fracao.divisao())
+def TCPserver(host, port):
+    tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server.bind((host, port))
+    tcp_server.listen()
+    conn, addr = tcp_server.accept()
 
-print(fracao.numerador)
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(data.decode('utf8'))
+
+        conn.sendall(data)
+    conn.close()
